a348811ba877a1b2bf11c65e68d66921aacf12a1
Link4Electronics <link4electronics@gmail.com>
2026-04-02 19:51:51 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,47 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+	cmake			\
+	kvantum 	    \
+	libass			\
+	libcdio			\
+	libgme		    \
+	libopenmpt		\
+    libsidplayfp    \
+	lxqt-qtplugin   \
+	ninja			\
+    pipewire-audio  \
+    pipewire-jack   \
+	qt6-5compat	    \
+	qt6-base 	    \
+	qt6-declarative \
+	qt6-svg  		\
+	qt6-tools		\
+	qt6ct			\
+	rubberband		\
+	shaderc			\
+	taglib			\
+	vulkan-headers
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building QMPlay2..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/zaps166/QMPlay2"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --recursive --depth 1 "$REPO" ./QMPlay2
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+cmake -S ./QMPlay2 -B build \
+	-G Ninja \
+	-DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
+    -DUSE_PCH=ON \
+    -DUSE_GIT_VERSION=ON
+cmake --build build -j$(nproc)
+cd ./build && ninja install
